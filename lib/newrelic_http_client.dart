7e3b268b72971ea40965e9df34056f1b4a56473c
SAGAR SURI <sagarsuri56@gmail.com>
2025-10-17 15:06:48 +0530

    fix: minimal null header guard, revert unrelated formatting

--- lib/newrelic_http_client.dart
+++ lib/newrelic_http_client.dart
@@ -1,5 +1,7 @@
 /*
+ *
  * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 import 'dart:async';
@@ -34,7 +36,7 @@ class NewRelicHttpClient implements HttpClient {
 
   @override
   set connectionFactory(
-      Future<ConnectionTask> Function(
+      Future<ConnectionTask<Socket>> Function(
               Uri url, String? proxyHost, int? proxyPort)?
           f) {
     client.connectionFactory = f;
@@ -79,15 +81,12 @@ class NewRelicHttpClient implements HttpClient {
 
   @override
   set authenticateProxy(
-      Future<bool> Function(
-              String host, int port, String scheme, String? realm)?
-          f) {
+      Future<bool> Function(String host, int port, String scheme, String? realm)? f) {
     client.authenticateProxy = f;
   }
 
   @override
-  set badCertificateCallback(
-      bool Function(X509Certificate cert, String host, int port)? callback) {
+  set badCertificateCallback(bool Function(X509Certificate cert, String host, int port)? callback) {
     client.badCertificateCallback = callback;
   }
 
@@ -98,101 +97,327 @@ class NewRelicHttpClient implements HttpClient {
 
   @override
   Future<HttpClientRequest> delete(String host, int port, String path) {
-    return _wrapRequest(() => client.delete(host, port, path));
+    return _wrapRequest(
+        client.delete(host, port, path), 'DELETE', Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> deleteUrl(Uri url) {
-    return _wrapRequest(() => client.deleteUrl(url));
+    return _wrapRequest(client.deleteUrl(url), 'DELETE', url);
   }
 
   @override
-  set findProxy(String Function(Uri url)? f) {
+  set findProxy(String Function(Uri uri)? f) {
     client.findProxy = f;
   }
 
   @override
   Future<HttpClientRequest> get(String host, int port, String path) {
-    return _wrapRequest(() => client.get(host, port, path));
+    return _wrapRequest(
+        client.get(host, port, path), 'GET', Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> getUrl(Uri url) {
-    return _wrapRequest(() => client.getUrl(url));
+    return _wrapRequest(client.getUrl(url), 'GET', url);
   }
 
   @override
   Future<HttpClientRequest> head(String host, int port, String path) {
-    return _wrapRequest(() => client.head(host, port, path));
+    return _wrapRequest(
+        client.head(host, port, path), 'HEAD', Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> headUrl(Uri url) {
-    return _wrapRequest(() => client.headUrl(url));
+    return _wrapRequest(client.headUrl(url), 'HEAD', url);
   }
 
   @override
-  Future<HttpClientRequest> open(
-      String method, String host, int port, String path) {
-    return _wrapRequest(() => client.open(method, host, port, path));
+  Future<HttpClientRequest> open(String method, String host, int port, String path) {
+    return _wrapRequest(
+        client.open(method, host, port, path), method, Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> openUrl(String method, Uri url) {
-    return _wrapRequest(() => client.openUrl(method, url));
+    return _wrapRequest(client.openUrl(method, url), method, url);
   }
 
   @override
   Future<HttpClientRequest> patch(String host, int port, String path) {
-    return _wrapRequest(() => client.patch(host, port, path));
+    return _wrapRequest(
+        client.patch(host, port, path), 'PATCH', Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> patchUrl(Uri url) {
-    return _wrapRequest(() => client.patchUrl(url));
+    return _wrapRequest(client.patchUrl(url), 'PATCH', url);
   }
 
   @override
   Future<HttpClientRequest> post(String host, int port, String path) {
-    return _wrapRequest(() => client.post(host, port, path));
+    return _wrapRequest(
+        client.post(host, port, path), 'POST', Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> postUrl(Uri url) {
-    return _wrapRequest(() => client.postUrl(url));
+    return _wrapRequest(client.postUrl(url), 'POST', url);
   }
 
   @override
   Future<HttpClientRequest> put(String host, int port, String path) {
-    return _wrapRequest(() => client.put(host, port, path));
+    return _wrapRequest(
+        client.put(host, port, path), 'PUT', Uri(scheme: 'http', host: host, port: port, path: path));
   }
 
   @override
   Future<HttpClientRequest> putUrl(Uri url) {
-    return _wrapRequest(() => client.putUrl(url));
+    return _wrapRequest(client.putUrl(url), 'PUT', url);
   }
 
-  Future<HttpClientRequest> _wrapRequest(
-      Future<HttpClientRequest> Function() requestFunction) async {
-    HttpClientRequest request = await requestFunction();
+  Future<HttpClientRequest> _wrapRequest(Future<HttpClientRequest> futureRequest, String method, Uri uri) async {
+    HttpClientRequest actualRequest = await futureRequest;
 
-    var traceAttributes = await NewrelicMobile.instance
-        .getTraceAttributes(request.uri.toString(), request.method);
+    Map<String, String?> traceAttributes = await NewRelicDtTrace.getTraceAttributes(uri.toString(), method);
+    List<String> availableTraceAttributesHeaders = [];
+    for (String header in traceAttributes.keys) {
+      availableTraceAttributesHeaders.add(header);
+    }
 
-    if (traceAttributes != null) {
-      if (traceAttributes['traceparent'] != null) {
-        request.headers.add('traceparent', traceAttributes['traceparent']!);
-      }
-
-      if (traceAttributes['tracestate'] != null) {
-        request.headers.add('tracestate', traceAttributes['tracestate']!);
-      }
-
-      if (traceAttributes['newrelic'] != null) {
-        request.headers.add('newrelic', traceAttributes['newrelic']!);
+    for (String header in availableTraceAttributesHeaders) {
+      final value = traceAttributes[header];
+      if (value != null) {
+        actualRequest.headers.add(header, value);
       }
     }
 
-    return request;
+    return NewRelicHttpClientRequest(actualRequest, uri, method, Config.getInstance());
   }
+}
+
+class NewRelicHttpClientRequest implements HttpClientRequest {
+  final HttpClientRequest request;
+  final String method;
+  final Uri uri;
+  final Config config;
+  String? contentType;
+  int? contentLength;
+  List<int>? bodyData;
+
+  NewRelicHttpClientRequest(this.request, this.uri, this.method, this.config);
+
+  @override
+  bool get bufferOutput => request.bufferOutput;
+
+  @override
+  set bufferOutput(bool bufferOutput) => request.bufferOutput = bufferOutput;
+
+  @override
+  int get contentLength => request.contentLength;
+
+  @override
+  set contentLength(int contentLength) {
+    this.contentLength = contentLength;
+    request.contentLength = contentLength;
+  }
+
+  @override
+  Encoding get encoding => request.encoding;
+
+  @override
+  set encoding(Encoding encoding) => request.encoding = encoding;
+
+  @override
+  bool get followRedirects => request.followRedirects;
+
+  @override
+  set followRedirects(bool followRedirects) => request.followRedirects = followRedirects;
+
+  @override
+  HttpHeaders get headers => request.headers;
+
+  @override
+  int get maxRedirects => request.maxRedirects;
+
+  @override
+  set maxRedirects(int maxRedirects) => request.maxRedirects = maxRedirects;
+
+  @override
+  bool get persistentConnection => request.persistentConnection;
+
+  @override
+  set persistentConnection(bool persistentConnection) => request.persistentConnection = persistentConnection;
+
+  @override
+  void abort([Object? exception, StackTrace? stackTrace]) {
+    request.abort(exception, stackTrace);
+  }
+
+  @override
+  void add(List<int> data) {
+    if (bodyData == null) {
+      bodyData = [];
+    }
+
+    bodyData!.addAll(data);
+    request.add(data);
+  }
+
+  @override
+  void addError(Object error, [StackTrace? stackTrace]) {
+    request.addError(error, stackTrace);
+  }
+
+  @override
+  Future addStream(Stream<List<int>> stream) {
+    return request.addStream(stream);
+  }
+
+  @override
+  Future<HttpClientResponse> close() async {
+    HttpClientResponse response = await request.close();
+    return NewRelicHttpClientResponse(response, this);
+  }
+
+  @override
+  HttpConnectionInfo? get connectionInfo => request.connectionInfo;
+
+  @override
+  List<Cookie> get cookies => request.cookies;
+
+  @override
+  Future<HttpClientResponse> done => request.done.then((response) => NewRelicHttpClientResponse(response, this));
+
+  @override
+  Future flush() {
+    return request.flush();
+  }
+
+  @override
+  String get method => request.method;
+
+  @override
+  Uri get uri => request.uri;
+
+  @override
+  void write(Object? obj) {
+    var data = obj.toString();
+    if (bodyData == null) {
+      bodyData = [];
+    }
+
+    bodyData!.addAll(utf8.encode(data));
+    request.write(obj);
+  }
+
+  @override
+  void writeAll(Iterable objects, [String separator = ""]) {
+    request.writeAll(objects, separator);
+  }
+
+  @override
+  void writeCharCode(int charCode) {
+    request.writeCharCode(charCode);
+  }
+
+  @override
+  void writeln([Object? obj = ""]) {
+    request.writeln(obj);
+  }
+}
+
+class NewRelicHttpClientResponse extends Stream<List<int>> implements HttpClientResponse {
+  final HttpClientResponse response;
+  final NewRelicHttpClientRequest request;
+  final Config config;
+  late int bytesReceived = 0;
+  late String? responseBody;
+  late List<int> bodyData;
+  bool isRedirect = false;
+
+  NewRelicHttpClientResponse(this.response, this.request) : config = request.config {
+    isRedirect = response.isRedirect;
+  }
+
+  @override
+  X509Certificate? get certificate => response.certificate;
+
+  @override
+  HttpClientResponseCompressionState get compressionState => response.compressionState;
+
+  @override
+  HttpConnectionInfo? get connectionInfo => response.connectionInfo;
+
+  @override
+  int get contentLength => response.contentLength;
+
+  @override
+  List<Cookie> get cookies => response.cookies;
+
+  @override
+  Future<Socket> detachSocket() {
+    return response.detachSocket();
+  }
+
+  @override
+  HttpHeaders get headers => response.headers;
+
+  @override
+  bool get isRedirect => response.isRedirect;
+
+  @override
+  StreamSubscription<List<int>> listen(void Function(List<int> event)? onData,
+      {Function? onError, void Function()? onDone, bool? cancelOnError}) {
+    bodyData = [];
+    return response.listen((data) {
+      bodyData.addAll(data);
+      bytesReceived += data.length;
+      if (onData != null) {
+        onData(data);
+      }
+    }, onError: onError, onDone: () {
+      if (bodyData.isNotEmpty) {
+        if (headers.contentType != null) {
+          try {
+            responseBody = utf8.decode(bodyData);
+          } catch (e) {
+            // ignore
+          }
+        }
+      }
+
+      NewrelicMobile.recordHttpTransaction(
+          request.method,
+          request.uri.toString(),
+          statusCode,
+          bytesReceived,
+          request.bodyData?.length ?? 0,
+          DateTime.now().millisecondsSinceEpoch,
+          DateTime.now().millisecondsSinceEpoch,
+          responseBody);
+
+      if (onDone != null) {
+        onDone();
+      }
+    }, cancelOnError: cancelOnError);
+  }
+
+  @override
+  bool get persistentConnection => response.persistentConnection;
+
+  @override
+  String get reasonPhrase => response.reasonPhrase;
+
+  @override
+  Future<HttpClientResponse> redirect([String? method, Uri? url, bool? followLoops]) {
+    return response.redirect(method, url, followLoops).then((response) => NewRelicHttpClientResponse(response, request));
+  }
+
+  @override
+  List<RedirectInfo> get redirects => response.redirects;
+
+  @override
+  int get statusCode => response.statusCode;
 }
