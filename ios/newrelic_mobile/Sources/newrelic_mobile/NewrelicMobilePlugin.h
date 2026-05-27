48f756583c5607082f756f1e6c64be0832cc8e46
ndesai <ndesai@newrelic.com>
2026-05-27 10:13:01 -0500

    [NR-528574]chore: Update native iOS agent to 7.7.2 and bump plugin version to 1.2.6
    - Updated NewRelic iOS agent version from 7.7.1 to 7.7.2 in `podspec` and `Package.swift`.
    - Bumped Flutter plugin version from 1.2.5 to 1.2.6 across `pubspec.yaml`, `version.dart`, and native source files.
    - Refactored iOS plugin structure: removed `NewrelicMobilePlugin.h/m`, renamed `SwiftNewrelicMobilePlugin.swift` to `NewrelicMobilePlugin.swift`, and added `@objc` exports for better compatibility.
    - Updated iOS example project to use `FlutterImplicitEngineDelegate` and increased minimum iOS deployment target to 15.0.
    - Updated Gradle wrapper to 8.11 in the example Android project.
    - Modified GitHub action workflow to support updating version strings in `Package.swift` and the renamed iOS plugin file.

--- ios/newrelic_mobile/Sources/newrelic_mobile/NewrelicMobilePlugin.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#import <Flutter/Flutter.h>
-
-@interface NewrelicMobilePlugin : NSObject<FlutterPlugin>
-@end
